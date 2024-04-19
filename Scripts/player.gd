0c307533fa618c3db0cea3f9650a516c60a36b82
Carbon <calumgcoppack@gmail.com>
2024-04-19 02:01:20 -0400

    Updated pickup hitboxes and player attacks

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,30 +9,30 @@ signal xp_changed(xp: int)
 @export var speed: int = 140
 @export var attack_damage: int = 1
 @export var inv_time: float = 1.0
+@export var shot_cooldown = 0.1
 
 @onready var health = starting_health
 @onready var itime = inv_time
 @onready var xp = 0
+@onready var cooldown: float = 0
 
 var level = 0
 var target_velocity = Vector2.ZERO
 var knife = preload("res://Scenes/flame_knife.tscn")
 
 func shoot():
-	for i in range(30):
-		var angle = randf_range(0, 2*PI)
-		var dir = Vector2.from_angle(angle)
-		var proj_node = knife.instantiate()
-		var proj: Projectile = proj_node.get_node(^"Projectile")
+	var dir = (get_global_mouse_position() - global_position).normalized()
+	var proj_node = knife.instantiate()
+	var proj: Projectile = proj_node.get_node(^"Projectile")
 
-		proj_node.global_position = global_position + dir * 10
-		proj_node.rotation = angle
-		
-		proj.damage_source = HurtBox.DamageSource.PLAYER
-		proj.damage = 3
-		proj.speed = 70
-		
-		get_parent().add_child(proj_node)
+	proj_node.global_position = global_position + dir * 10
+	proj_node.rotation = dir.angle() 
+	
+	proj.damage_source = HurtBox.DamageSource.PLAYER
+	proj.damage = 3
+	proj.speed = 70
+	
+	get_parent().add_child(proj_node)
 
 func gain_xp(experience: int) -> void:
 	if experience >= 0:
@@ -55,11 +55,13 @@ func _ready() -> void:
 	xp_changed.emit(xp)
 
 func _process(delta: float) -> void:
-	if itime > 0:
-		itime = max(0, itime - delta)
+	if cooldown > 0: cooldown = max(0, cooldown - delta)
+	if itime > 0: itime = max(0, itime - delta)
 		
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_pressed("attack") && cooldown == 0:
+		cooldown += shot_cooldown
 		shoot()
+		
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector2.ZERO
